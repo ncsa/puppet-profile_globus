f8c3a6e2c3a8f8592c48278fe0569c5ad6065550
Sean Stevens <sstevens@illinois.edu>
2022-05-05 12:43:00 -0500

    typo

--- manifests/custom_gridftp.pp
+++ manifests/custom_gridftp.pp
@@ -5,7 +5,7 @@
 # @example
 #   include profile_globus::custom_gridftp
 class profile_globus::custom_gridftp (
-  string     $conf='/etc/gridftp.d/custom_gridftp_conf',
+  String     $conf='/etc/gridftp.d/custom_gridftp_conf',
 ){ 
     file { $conf:
         content => 'control_interface 127.0.0.1',
