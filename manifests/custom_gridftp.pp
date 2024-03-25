41f7f6541b08fc2f1fc35baf9f5ba41e6f97aa01
Sean Stevens <sstevens@illinois.edu>
2024-03-25 13:48:46 -0500

    linting fixes

--- manifests/custom_gridftp.pp
+++ manifests/custom_gridftp.pp
@@ -5,23 +5,23 @@
 # @example
 #   include profile_globus::custom_gridftp
 class profile_globus::custom_gridftp (
-  String     $conf='/etc/gridftp.d/custom_gridftp_conf',
   Hash       $conf_lines,
+  String     $conf='/etc/gridftp.d/custom_gridftp_conf',
 ) {
   file { $conf:
-    ensure  => file,
-    group   => root,
-    mode    => '0644',
-    owner   => root,
-    notify  => Service['globus-gridftp-server'],
+    ensure => file,
+    group  => root,
+    mode   => '0644',
+    owner  => root,
+    notify => Service['globus-gridftp-server'],
   }
 
   if ( ! empty($conf_lines) ) {
     $conf_lines.each | String $key, String $value | {
-      file_line { "ensure $key $value in $conf":
+      file_line { "ensure ${key} ${value} in ${conf}":
         ensure => present,
         path   => $conf,
-        line   => "$key $value",
+        line   => "${key} ${value}",
         match  => $key,
         notify => Service['globus-gridftp-server'],
       }
